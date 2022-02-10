2f30ab866b45f9457cf107c0ae7faf02d65c48b7
William Chan <wchan244@se12l0325-13.ad.bcit.ca>
2022-02-09 17:27:46 -0800

    time checking

--- AnimalPark/SceneDelegate.swift
+++ AnimalPark/SceneDelegate.swift
@@ -23,14 +23,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead)
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive
+        
+        calculateElapsedTime()
+        dateLabelTest.text = refreshData.elapsedTime.description + " seconds since last opened"
     }
 
+    func calculateElapsedTime() {
+        refreshData.elapsedTime = Int(Date().timeIntervalSinceReferenceDate) - refreshData.lastOpened
+    }
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
@@ -45,6 +52,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        refreshData.lastOpened = Int(Date().timeIntervalSinceReferenceDate)
     }
 
 
--- AnimalPark/ViewController.swift
+++ AnimalPark/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+let dateLabelTest = UILabel()
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        refreshData.lastOpened = Int(Date().timeIntervalSinceReferenceDate)
+        
+        dateLabelTest.frame = CGRect(x: 15, y: 15, width: 300, height: 200)
+        self.view.addSubview(dateLabelTest)
     }
 
-
+    
 }
 
+struct refreshData {
+    static var elapsedTime = 0
+    static var lastOpened = 0
+}
