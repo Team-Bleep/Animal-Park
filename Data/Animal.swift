f0ee142047a660aec8bbf8ca5688295e5e951fd2
glowiigno <gloria.nhuto.ngo@gmail.com>
2022-02-23 03:45:14 -0800

    added animal, store, toy

--- Data/Animal.swift
+++ Data/Animal.swift
@@ -7,7 +7,23 @@
 
 import Foundation
 
-class Animal {
+struct Animal {
+    /// Rarity of the animal spawning
+    let rarity: Int
+    /// Amount of value in exp that the animal will grant the player
+    let value: Int
+    /// Position of the animal
+    var position: (Double, Double)
+    /// Whether an animal is currently playing with a toy at the moment
+    var playing: Bool
+    /// If the animal is playing with a toy, toy object the animal is playing with
+    var toy: Toy?
     
-    
+    init(rarity: Int, value: Int, position: (Double, Double)) {
+        self.rarity = rarity
+        self.value = value
+        self.position = position
+        self.playing = false
+        self.toy = nil
+    }
 }
